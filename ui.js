beedb6ba8b1c179cdb70b04867c3cde048d7aebc
bpw90x <bpw90x@gmail.com>
2019-10-19 18:14:36 -0400

    Working Display

--- ui.js
+++ ui.js
@@ -5,7 +5,36 @@ class UI {
 
   showProfile(user) {
     console.log(user);
+    this.profile.innerHTML = `
+      <div class="card card-body mb-3">
+        <div class="row">
+          <div class="col-md-3">
+            <img class="img-fluid mb-2" src="${user.avatar_url}">  
+            <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
+            </div>
+            <div class="col-md-9">
+              <a href="${user.repos_url}" target="_blank"
+                span class="badge badge-primary">Public Repos: ${user.public_repos}</a>
+              <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
+              <span class="badge badge-success">Followers: ${user.followers}</span>
+              <span class="badge badge-info">Following: ${user.following}</span>
+              <br<br>
+              <ul class ="list-group">
+              <li class="list-group-item">Company: ${user.company}</li>
+              <li class="list-group-item">Website/Blog: ${user.blog}</li>
+              <li class="list-group-item">Location: ${user.location}</li>
+              <li class="list-group-item">Member Since: ${user.created_at}</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <h3 class="page-heading mb-3">Latest Repos</h3>
+        <div id="repos"></div>
+    `;
+
    
 
   }
 }
+
+//viewing link https://api.github.com/users/bpw90x we can see the data that we have available, such as avatar_url / gravatar_id, etc.
